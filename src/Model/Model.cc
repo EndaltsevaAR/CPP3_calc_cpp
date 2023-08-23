04cdf02ce92853362d4ef59ccb2faecfc38bf4da
joserans <joserans@student.21-school.ru>
2023-08-24 00:37:40 +0300

    posifix start

--- src/Model/Model.cc
+++ src/Model/Model.cc
@@ -1,4 +1,7 @@
+#include <vector>
+#include <list>
 #include "iostream"
+#include "list"
 
 #include "Model.h"
 
@@ -13,18 +16,41 @@ namespace s21 {
 
         std::string posifix = to_posifix(expression, x);
 
-        бла
+
 
 
         return std::string();
     }
 
-    std::string Model::to_posifix(const std::string &expression, const std::string &x) {
-        std::string posifix_expression;
+    std::string Model::to_posifix(const std::string &infix, const std::string &x) {
+        bool flag_error = false;
+        std::string posifix;
+        std::list<std::string> char_list;
 
 
+        for (size_t infix_count = 0, postfix_count = 0; infix_count < infix.size() && !flag_error; ++infix_count) {
+            if(isdigit(infix[infix_count])) { // digit check
+                posifix[postfix_count] = infix[infix_count];
+                ++postfix_count;
+                if (infix_count + 1 < infix.size() && (!(isdigit(infix[infix_count+1]) || infix[infix_count+1] == '.'))) {
+                    posifix[postfix_count] = ' ';
+                    ++postfix_count;
+                }
+            } else if (infix[infix_count] == '.') {
+                if (infix_count > 0 && isdigit(infix[infix_count-1]) && isdigit(infix[infix_count+1])) {
+                    posifix[postfix_count] = infix[infix_count+1];
+                    ++postfix_count;
+                } else {
+                    std::cout<< "Dote is not correct!" << std::endl;
+                    flag_error = true;
+                }
+            }
+        }
+        if (flag_error) {
+            posifix = ERROR;
+        }
 
-        return std::string();
+        return posifix;
     }
 
 
